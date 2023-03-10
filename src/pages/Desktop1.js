15693a6c46ae59f4c0498f179cf9b2f0986fc18e
Harshil1611 <110885313+Harshil1611@users.noreply.github.com>
2023-03-10 11:52:57 +0530

    cmt1

--- src/pages/Desktop1.js
+++ src/pages/Desktop1.js
@@ -2,52 +2,29 @@ import "./Desktop1.css";
 
 const Desktop1 = () => {
   return (
+    
+    <body className="body">
+      
     <div className="desktop-1">
-      <div className="desktop-1-child" />
-      <img className="desktop-1-item" alt="" src="../rectangle-4.svg" />
-      <img className="desktop-1-inner" alt="" src="../line-3.svg" />
-      <img className="desktop-1-inner" alt="" src="../line-1.svg" />
-      <img className="desktop-1-child1" alt="" src="../line-6.svg" />
-      <img className="desktop-1-child2" alt="" src="../line-4.svg" />
-      <img className="desktop-1-child3" alt="" src="../line-5.svg" />
-      <img className="desktop-1-child4" alt="" src="../line-2.svg" />
-      <div className="rectangle-div" />
-      <div className="login">LOGIN</div>
-      <img className="rectangle-icon" alt="" src="../rectangle-3.svg" />
-      <div className="login1">
-        <div className="login2">LOGIN</div>
-      </div>
-      <div className="forgot-password">Forgot Password.</div>
-      <div className="clap-logo">
-        <div className="mask-group">
-          <div className="mask-group">
-            <div className="clap">CLAP</div>
+          <img className="gif-Logo" src="../CLAP1.gif"></img>
+          <h2>Enter Your Details</h2>
+          <img className="svg-user" alt="" src="../user.svg" />
+          <img className="svg-key" alt="" src="../key.svg" />
+          <div class="box">
+                
+                <input type="email" name="email" id="email" placeholder="Enter your Login id" ></input>
+                <input type="password" name="password" id="password" placeholder="Enter password" ></input>
+                <h1 className="txt-forgotpassword">Forgot Password</h1>
+                <button className="btn-signin" type="submit">SIGN IN</button>
+          </div>\
+            <h1 className="txt-or">Signup </h1>
+          <div className="login-methods">
+          <img className="svg-google" alt="" src="../facebook-f.svg" />
+          <img className="svg-google" alt="" src="../github.svg" />
+          <img className="svg-google" alt="" src="../google.svg" />
           </div>
-          <div className="component-2-child" />
-        </div>
-      </div>
-      <div className="submit-button">
-        <img className="submit-button-child" alt="" src="../rectangle-6.svg" />
-        <div className="submit">SUBMIT</div>
-      </div>
-      <div className="password">
-        <div className="password1">PASSWORD</div>
-        <img className="vector-icon" alt="" src="../vector.svg" />
-        <div className="password-child" />
-        <img
-          className="pheye-closed-bold-icon"
-          alt=""
-          src="../pheyeclosedbold.svg"
-        />
-      </div>
-      <div className="component-3">
-        <div className="component-3-child" />
-        <div className="username">USERNAME</div>
-        <img className="vector-icon1" alt="" src="../vector1.svg" />
-      </div>
-      <img className="desktop-1-child5" alt="" src="../rectangle-7.svg" />
-      <img className="desktop-1-child6" alt="" src="../rectangle-8.svg" />
     </div>
+    </body>
   );
 };
 
